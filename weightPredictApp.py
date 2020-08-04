baf9b83e33c01e4c1f8d5af1bec186991f4f6726
Wade Bick <wade.bick@gmail.com>
2020-08-03 21:54:09 -0500

    Basic SQL put and get instructions debugged

--- weightPredictApp.py
+++ weightPredictApp.py
@@ -2,47 +2,69 @@ import numpy as np
 import pandas as pd
 import datetime as dt
 import streamlit as st
-#import mysql.connector
-#from mysql.connector import errorcode
-#from darts import TimeSeries
-#from darts.models import AutoARIMA
-#from darts.models import NaiveDrift
+from sqlalchemy import create_engine
+import pymysql
+from darts import TimeSeries
+from darts.models import AutoARIMA
+from darts.models import NaiveDrift
 # Change to multi-model where if <30 days, use naive_drift, naive_mean, or RegressionModel
 #   If > 30 days, allow to choose AutoARIMA or others
-#import <some kind of sql library
 
-global glMODEL, glSqlCursor, glWeightDB
+global glMODEL, glWeightDB
 #glMODEL = NaiveDrift()
 glSqlCursor = None
-glWeightDB = mysql.connector.connect(
+sqlEngine       = create_engine('mysql+pymysql://root:adminadmin@127.0.0.1:3306/date_weight_pairs', echo=True)
+try:
+    glWeightDB    = sqlEngine.connect()
+except:
+    print("ERROR: Couldn't connect to database")
+'''glWeightDB = mysql.connector.connect(
     host="127.0.0.1",
     user="root",
     passwd="adminadmin",
     database="date_weight_pairs"
-)
-todaysEXPTDate = dt.date(2020, 7, 30)
+)'''
+todaysEXPTDate = dt.date(2020, 7, 31)
 
 todaysDate = dt.date.today()
 
 #These 2 imply hardcoding the server and database.
-#Make those constants for now, maybe provide a user param later
-#Maybe a DB object that stores server address, DB name, etc
-def sqlStoreWeight(currentDate, weight):
-    sqlCursor = glWeightDB.cursor(buffered=True)
-    sql = "INSERT INTO dateWeightTable VALUES (%s, %s)"
-    val = (currentDate, weight)
-    sqlCursor.execute(sql, val)
-    glWeightDB.commit()
-    # With sendToSQLDB we got a return status. Will .commit() give that?
-        #insertTableStr = "INSERT INTO " + self.TableName + " VALUES(" + str(listOfValues)[1:-1] + ");"
-        #success = self.sendToSQLDB(insertTableStr, "Insert into SQL table")
-        #return success
+''' Notes on using SQL Alchemy and the pandas .to_sql command:
+
+    It seems to be common in pandas to just use a default index column. The pandas index is treated as the SQL key.
+    So that's like saying we'll just juse a default, autoincrement PK. When designing SQL tables,
+    that's not always the case. It's common to use a specific, unique value like transaction ID, username, etc.
+    There's likely to be some significance to the PK so a simple AUTO_INCREMENT on the PK column isn't sufficient.
+    If that's the case, where we're setting specific values for the PK, then we need to make sure pandas is treating
+    the corresponding column as the index and not using an auto generated index column. Also, make sure that index=True
+    when calling .to_sql
+
+    This also brings up an interesting case with composite PKs. Say we have 3 columns in our PK. Only pandas > 1.0+ have
+    multi-index capability (I think). Not sure how this would work. Might be interesting to try. If it doesn't work, then
+    would have to manually create the SQL string and use SQL Alchemy's .execute command. That also leads to the question,
+    "If I'm just going to use .execute, when not just use pymysql? Or the MySQL python connector?"
+'''
+def sqlStoreWeight(currentDateObj, weight):
+    tempDateStr = currentDateObj.strftime('%Y-%m-%d')
+    tempDF = pd.DataFrame({'date':tempDateStr, 'weight':weight}, index=[0]) # If only passing scalars, have to specify an index value
+                                                                            # Index takes an array-like, so can't just use 'date'
+                                                                            # Specifying the date value will result in that value
+                                                                            # for both the index and the 'date' column. Easiest to
+                                                                            # just give a dummy array value like [0]
+    tempDF.set_index('date', inplace=True)
+
+    try:
+        tempDF.to_sql('dateweighttable', glWeightDB,if_exists='append', index=True)
+        '''sqlStr = "INSERT INTO dateweighttable (date, weight) VALUES (\'{}\', {})".format(currentDateObj.strftime('%Y-%m-%d'), weight)
+        glWeightDB.execute(sqlStr)'''
+    except:
+        print("ERROR: writing date and weight data to database")
+        if ValueError:
+            print("ERROR: ValueError writing date and weight data to database")
     return
 #@st.cache
 def sqlGetWeightsDF():
-    sqlCursor = glWeightDB.cursor(buffered=True)
-    sqlCursor.execute("SELECT * FROM dateWeightTable")
-    sqlDF = pd.DataFrame(sqlCursor.fetchall())
+    sqlDF = pd.read_sql_table('dateweighttable', glWeightDB, columns=['date', 'weight'])
     return sqlDF
 
 #@st.cache
@@ -72,7 +94,7 @@ st.selectbox("Who's weight are we working with?",('Wade', 'Larry', 'Mo', 'Curly'
 weight = st.number_input("Today's weight:", min_value=50, max_value=750, format="%d")
 st.write("Echoing weight: ", weight)
 
-sqlStoreWeight(weight)
+sqlStoreWeight(todaysEXPTDate,weight)
 #weightsDF = pd.DataFrame({'date':['7-17-2020','7-18-2020','7-19-2020','7-20-2020','7-21-2020','7-22-2020'],
 #                          'weight':[170.8, 170.4, 170.8, 170.6, 170.6, 745]})
 weightsDF = sqlGetWeightsDF() # Would passing todaysEXPTDate here be helpful?

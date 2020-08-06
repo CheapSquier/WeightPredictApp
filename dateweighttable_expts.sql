17dcd50b20612607c029a162fdec324b936d902c
Wade Bick <wade.bick@gmail.com>
2020-08-06 12:14:52 -0500

    Basically functional, but lots on the TODO list

--- dateweighttable_expts.sql
+++ dateweighttable_expts.sql
@@ -23,29 +23,47 @@ END;
 
 INSERT INTO dateweighttable (date, weight) 
 	VALUES
-		('2020-7-17', 170.8), 
 		('2020-7-18', 170.4), 
 		('2020-7-19', 170.8), 
 		('2020-7-20', 170.6), 
 		('2020-7-21', 170.6), 
-		('2020-7-22', 745.0);
+		('2020-7-22', 170.6),
+		('2020-7-23', 170.0), 
+		('2020-7-24', 170.4), 
+		('2020-7-25', 170.4), 
+		('2020-7-26', 170.6), 
+		('2020-7-27', 170.8),
+		('2020-7-28', 170.4), 
+		('2020-7-29', 168.6), 
+		('2020-7-30', 169.6), 
+		('2020-7-31', 170.4), 
+		('2020-8-01', 170.2),
+		('2020-8-02', 170.2);
 COMMIT;
 
 DELETE FROM dateweighttable  
 	WHERE date IN
 		('2020-7-17', '2020-7-18', '2020-7-19', '2020-7-20', '2020-7-21',
-        '2020-7-22');
+        '2020-7-22', '2020-7-30', '2020-7-31');
+COMMIT;
+
+-- This won't work since we have safe update mode enabled
+DELETE FROM dateweighttable;
 COMMIT;
 
 INSERT INTO dateweighttable (date, weight) VALUES ('2020-07-31', 50);
 
 INSERT INTO dateweighttable VALUES('2020-07-30', 50);
 
-DELETE FROM dateweighttable WHERE date='2020-07-31';
+DELETE FROM dateweighttable WHERE date='2020-08-04';
 
-BEGIN;
 INSERT INTO users (`index`, name, color) VALUES (%(index)s, %(name)s, %(color)s)
 ({'index': 0, 'name': 'User 1', 'color': 'purple'}, 
  {'index': 1, 'name': 'User 2', 'color': 'rose'}, 
  {'index': 2, 'name': 'User 3', 'color': 'yellow'});
 COMMIT;
+
+SELECT date FROM date_weight_pairs.dateweighttable
+WHERE EXISTS (SELECT date FROM date_weight_pairs.dateweighttable WHERE date='2020-07-19');
+
+SELECT date FROM date_weight_pairs.dateweighttable WHERE date='2020-7-19';
--- weightPredictApp.py
+++ weightPredictApp.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import datetime as dt
 import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
 from sqlalchemy import create_engine
 import pymysql
 from darts import TimeSeries
@@ -10,10 +12,9 @@ from darts.models import NaiveDrift
 # Change to multi-model where if <30 days, use naive_drift, naive_mean, or RegressionModel
 #   If > 30 days, allow to choose AutoARIMA or others
 
-global glMODEL, glWeightDB
-#glMODEL = NaiveDrift()
+global glModel, glWeightDB
 glSqlCursor = None
-sqlEngine       = create_engine('mysql+pymysql://root:adminadmin@127.0.0.1:3306/date_weight_pairs', echo=True)
+sqlEngine       = create_engine('mysql+pymysql://root:adminadmin@127.0.0.1:3306/date_weight_pairs', echo=False)
 try:
     glWeightDB    = sqlEngine.connect()
 except:
@@ -24,11 +25,9 @@ except:
     passwd="adminadmin",
     database="date_weight_pairs"
 )'''
-todaysEXPTDate = dt.date(2020, 7, 31)
-
+#todaysDate = dt.date(2020, 9, 3) # Use this for debugging
 todaysDate = dt.date.today()
 
-#These 2 imply hardcoding the server and database.
 ''' Notes on using SQL Alchemy and the pandas .to_sql command:
 
     It seems to be common in pandas to just use a default index column. The pandas index is treated as the SQL key.
@@ -45,6 +44,8 @@ todaysDate = dt.date.today()
     "If I'm just going to use .execute, when not just use pymysql? Or the MySQL python connector?"
 '''
 def sqlStoreWeight(currentDateObj, weight):
+    # Check if this date already exits in DB. If yes, switch to UPDATE (which also means don't use to_sql, since that would overwrite the whole table
+    # If just using INSERT, this would be a PK error
     tempDateStr = currentDateObj.strftime('%Y-%m-%d')
     tempDF = pd.DataFrame({'date':tempDateStr, 'weight':weight}, index=[0]) # If only passing scalars, have to specify an index value
                                                                             # Index takes an array-like, so can't just use 'date'
@@ -54,33 +55,45 @@ def sqlStoreWeight(currentDateObj, weight):
     tempDF.set_index('date', inplace=True)
 
     try:
-        tempDF.to_sql('dateweighttable', glWeightDB,if_exists='append', index=True)
-        '''sqlStr = "INSERT INTO dateweighttable (date, weight) VALUES (\'{}\', {})".format(currentDateObj.strftime('%Y-%m-%d'), weight)
-        glWeightDB.execute(sqlStr)'''
+        #tempDF.to_sql('dateweighttable', glWeightDB,if_exists='append', index=True)
+        if sqlCheckDateExists(currentDateObj):
+            sqlStr = "UPDATE dateweighttable SET weight = {} WHERE date = \'{}\'".format(weight, currentDateObj.strftime('%Y-%m-%d'))
+        else:
+            sqlStr = "INSERT INTO dateweighttable (date, weight) VALUES (\'{}\', {})".format(currentDateObj.strftime('%Y-%m-%d'), weight) 
+        glWeightDB.execute(sqlStr)
     except:
         print("ERROR: writing date and weight data to database")
         if ValueError:
             print("ERROR: ValueError writing date and weight data to database")
     return
+
 #@st.cache
 def sqlGetWeightsDF():
     sqlDF = pd.read_sql_table('dateweighttable', glWeightDB, columns=['date', 'weight'])
     return sqlDF
 
+def sqlCheckDateExists(dateObj):
+    sqlStr = "SELECT date FROM date_weight_pairs.dateweighttable WHERE date = \'{}\'".format(dateObj.strftime('%Y-%m-%d'))
+    response = glWeightDB.execute(sqlStr)
+    if response.rowcount == 0:
+        return False
+    else:
+        return True
+
 #@st.cache
 def trainTheModel(training_data):
-    glMODEL.fit(training_data)
+    glModel.fit(training_data)
     print("Training...")
     return
 
 #@st.cache
 def predict_next(days=3):
-    next3days = glMODEL.predict(days)
+    next3days = glModel.predict(days)
     print("Predicting...")
     return next3days.pd_dataframe()
 
+st.title("Weight Prediction App")
 st.write("""
-# Weight Prediction App
 No magic here, just looking for trends and cycles in an individual's body weight.
 The app will allow no more than one weight value per day.
 """)
@@ -91,25 +104,48 @@ st.write("""
 
 st.selectbox("Who's weight are we working with?",('Wade', 'Larry', 'Mo', 'Curly'),0)
 
-weight = st.number_input("Today's weight:", min_value=50, max_value=750, format="%d")
+weight = st.number_input("Today's weight:", min_value=50.0, max_value=750.0, format="%4.1f", step=.1)
 st.write("Echoing weight: ", weight)
 
-sqlStoreWeight(todaysEXPTDate,weight)
+sqlStoreWeight(todaysDate,weight)
 #weightsDF = pd.DataFrame({'date':['7-17-2020','7-18-2020','7-19-2020','7-20-2020','7-21-2020','7-22-2020'],
 #                          'weight':[170.8, 170.4, 170.8, 170.6, 170.6, 745]})
 weightsDF = sqlGetWeightsDF() # Would passing todaysEXPTDate here be helpful?
+if weightsDF.date.size > 30:  # Remember, size is rows*cols, so just get the size of a series or the index of the DF
+    glModel = AutoARIMA()
+else:
+    glModel = NaiveDrift()
 
-series = TimeSeries.from_dataframe(weightsDF, 'date', 'weight')
+series = TimeSeries.from_dataframe(weightsDF, time_col='date', value_cols='weight')
 
-train, val = series.split_after(pd.Timestamp(todaysEXPTDate))
+train, val = series.split_after(pd.Timestamp(todaysDate - dt.timedelta(days=3)))
 
 trainTheModel(train)
 
-next3daysDF = predict_next(3)
+next3daysDF = predict_next(4)
 print(next3daysDF[0])
 
-totalPlotDF = weightsDF + next3daysDF
-st.line_chart(weightsDF.weight)
-st.write("*Tomorrows weight expected to be: *", next3daysDF[0][0])
+plotYmax = weightsDF['weight'].max() + 0.5
+plotYmin = weightsDF['weight'].min() - 0.5
+weightMarker = go.scatter.Marker(symbol="circle", size=8, color='blue')
+# Super simple chart, very minimal customization options
+#st.line_chart(weightsDF.weight)
+# Plotly Express is better, but can't currently add markers to the line
+weightFig = px.line(weightsDF, x='date', y='weight', title="Past and Predicted Weights", range_y=[plotYmin, plotYmax])
+weightFig.add_scatter(x=weightsDF['date'], y=weightsDF['weight'], mode='markers', marker=weightMarker, name="Actual")
+weightFig.add_scatter(x=next3daysDF.index, y=next3daysDF[0], name="Predicted")
+weightFig.update_layout(showlegend=True)
+# So move on the full Plotly Graph Objects
+st.plotly_chart(weightFig, use_container_width = False)
+#outputStr = "*Based on data from the last {} days, omorrow's weight is expected to be: {}*".format(weightsDF.date.size, next3daysDF[0][3])
+st.write("*Based on data from the last {} days, tomorrow's weight is expected to be: {:4.1f}*".format(weightsDF.date.size, next3daysDF[0][3]))
 
 
+'''
+TODO: Handle time when days are between 1-3 and 3-6 (need to adjust # days in validation trace)
+TODO: Add multi-user support
+TODO: Limit number of days on the graph (x-axis) to... 30? 45?
+TODO: Store previous predictions in DB, plot them, and calculate an MSE or something similar
+TODO: SQL errors should be handled and sent to the app so the user can report them
+TODO: Pull default weight from SQL so the current 50 default doesn't show up in another browser or during refresh. 50 should only occur when no weight has ever been entered
+'''
